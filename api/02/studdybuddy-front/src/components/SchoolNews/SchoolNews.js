17abd09d82df98c0d4d58d0f789d55ad8a8a201b
astropaula <wojciechowska.fran@gmail.com>
2021-05-23 22:29:20 +0200

    added api to shoolnews section

--- api/02/studdybuddy-front/src/components/SchoolNews/SchoolNews.js
+++ api/02/studdybuddy-front/src/components/SchoolNews/SchoolNews.js
@@ -1,45 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Article, ArticleImage, Articles, Title, Wrapper } from 'components/SchoolNews/SchoolNews.styles';
 
+const DATA_TOKEN= 'd0b07ea7c7d6f474f87a8dac0b2b50';
+const query = `{
+  allArticles {
+    title
+    content
+    image {
+      alt
+      url
+    }
+  }
+}`;
 
-const SchoolNews = () => (
-  <Wrapper>
+const SchoolNews = () => {
+  const[articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${DATA_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query,
+      })
+    })
+    .then(res => res.json())
+    .then(({ data: { allArticles } }) => setArticles(allArticles))
+    .catch(err => console.log(err));
+  }, []);
+  return (
+    <Wrapper>
     <Title>Gazetka szkolna</Title>
     <Articles>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-        <h3>Wymiana komputerów</h3>
-        <p>Już niebawem w sali informatycznej 118
-          pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
+      {articles ? articles.map(article => (
+        <Article>
+          <ArticleImage>
+            <img src={article.image.url} alt={article.image.alt} />
+          </ArticleImage>
+          <div>
+            <h3>{article.title}</h3>
+            <p>{article.content}</p>
+          </div>
+        </Article>
+      )) : (
+        <h2>no articles</h2>
+      )}
     </Articles>
   </Wrapper>
-);
+  )
+};
 
 SchoolNews.propTypes = {};
 
